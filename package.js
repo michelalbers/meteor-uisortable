6569b47c1bed64428964547ceec4bc02b978cff5
Michel Albers <Michel.Albers@sophos.com>
2015-11-03 11:12:36 +0100

    Update dependencies

--- package.js
+++ package.js
@@ -1,18 +1,13 @@
 Package.describe({
   name: 'interwebs:ui-sortable',
   version: '1.0.0',
-  // Brief, one-line summary of the package.
   summary: 'Wrapper for angular ui-sortable (https://github.com/angular-ui/ui-sortable)',
-  // URL to the Git repository containing the source code for this package.
-  git: '',
-  // By default, Meteor will default to using README.md for documentation.
-  // To avoid submitting documentation, set this field to null.
-  documentation: null
+  git: 'https://github.com/michelalbers/meteor-uisortable',
 });
 
 Package.onUse(function(api) {
   api.versionsFrom('1.1.0.3');
-  api.use('urigo:angular');
-  api.use('mizzao:jquery-ui');
+  api.use('urigo:angular@1.0.4');
+  api.use('mizzao:jquery-ui@1.11.4');
   api.addFiles('sortable.js', ['client']);
 });
